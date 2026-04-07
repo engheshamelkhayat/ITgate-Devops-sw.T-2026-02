2f688a35ff09c3a1ea2ab25eec68c96e611666a4
engheshamelkhayat <dengheshamelkhayat898@gmail.com>
2026-04-07 15:41:03 +0200

    Add two function add, gitMax

--- src/main.c
+++ src/main.c
@@ -1,8 +1,16 @@
 
 #include <studio>
+
+double add(double a, double b) {
+    return a+b;
+}
+double getmax(double a, double b) {
+    return (a > b) ? a : b;
+}
 int main(void){
 int a=10;
 int b=20;
-ptintf("sum=%d\n",a+b);
+ptintf("sum= %d \n", add(a+b));
+ptintf("Max= %d \n", getmax(a+b));
 return0;
 }
