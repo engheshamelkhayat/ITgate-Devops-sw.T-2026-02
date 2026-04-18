91c6950006630d83946090a52292aea07d416950
engheshamelkhayat <dengheshamelkhayat898@gmail.com>
2026-04-18 14:20:33 +0200

    Add New substract function

--- src/main.c
+++ src/main.c
@@ -7,11 +7,15 @@ double add(double a, double b) {
 double getmax(double a, double b) {
     return (a > b) ? a : b;
 }
+double sub(double a,double b){
+ return a-b; 
+}
 int main(void){
 int a=10;
 int b=20;
 printf("Hello Tream");
 ptintf("sum= %d \n", add(a+b));
 ptintf("Max= %d \n", getmax(a+b));
+printf("subtraction=%f",subtract(a,b));
 return0;
 }
